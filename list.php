cdddc9faae20efffad80d371691ed3eecb9a1c7c
Luciano <>
2025-10-21 22:39:36 -0300

    feat: filter backend

--- list.php
+++ list.php
@@ -25,14 +25,12 @@
         <legend>Filter</legend>
         <form action="list.php" method="GET">
             <select id="sortby" name="sort">
-                <option value="0" selected disabled>Sort By</option>
                 <option value="1">Name</option>
                 <option value="2">Date</option>
                 <option value="3">Size</option>
                 <option value="4">Type</option>
             </select>
             <select id="orderby" name="order">
-                <option value="0" selected disabled>Order By</option>
                 <option value="1">Ascending</option>
                 <option value="2">Descending</option>
             </select>
@@ -43,21 +41,86 @@
     <main class="container">
         <ul class="u-list">
             <?php 
+                $basedir=__DIR__.'/uploads/';
                 function humanizeBytes($bytes, $decimals = 2) {
                     $size = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
                     $factor = floor((strlen($bytes) - 1) / 3);
                     return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor))." ".@$size[$factor];
                 }
                 /* You should already sanitize the filename when upload */
-                $basedir=__DIR__.'/uploads/';
-                foreach(new DirectoryIterator($basedir) as $name){
-                    $fullpath=$basedir.$name;
-                    if( $name->isDot() ) continue;
-                    if( ! $name->isFile() ) continue;
-                    $type=mime_content_type($fullpath);
-                    $size=humanizeBytes($name->getSize());
-                    
-                    $ctime=date('Y-m-d H:i:s',$name->getCTime());
+                function get_array_files() {
+                    $ret=[];
+                    global $basedir;
+                    foreach(new DirectoryIterator($basedir) as $entity){
+                        if( $entity->isDot() ) continue;
+                        if( !$entity->isFile() ) continue;
+                        $obj=[
+                            'name'=>$entity->getFilename(),
+                            'type'=>mime_content_type($basedir.$entity),
+                            'size'=>humanizeBytes($entity->getSize()),
+                            'bytes'=>$entity->getSize(),
+                            'ctime'=>$entity->getCTime(),
+                            'created'=>date('Y-m-d H:i:s',$entity->getCTime())
+                        ];
+                        array_push($ret,$obj);
+                    }
+                    return $ret;
+                }
+                function sort_array_files(&$arr,$sort='1',$order='1'){
+                    switch($sort){
+                        case '1':
+                            usort($arr,function($x,$y){
+                                global $order;
+                                if( $order === '2' )
+                                    [$x,$y]=[$y,$x];
+
+                                return strcasecmp($x['name'],$y['name']);
+                            });
+                            break;
+                        case '2':
+                            usort($arr,function($x,$y){
+                                global $order;
+                                $_x=intval($x['ctime']);
+                                $_y=intval($y['ctime']);
+                                if( $order === '2' )
+                                    [$_x,$_y]=[$_y,$_x];
+                                    
+                                return $_x - $_y ;
+                            });
+                            break;
+                        case '3':
+                            usort($arr,function($x,$y){
+                                global $order;
+                                $_x=intval($x['bytes']);
+                                $_y=intval($y['bytes']);
+                                if( $order === '2' )
+                                    [$_x,$_y]=[$_y,$_x];
+                                
+                                return $_x - $_y ;
+                            });
+                            break;
+                        case '4':
+                            usort($arr,function($x,$y){
+                                global $order;
+                                if( $order === '2' )
+                                    [$x,$y]=[$y,$x];
+
+                                return strcasecmp($x['type'],$y['type']);
+                            });
+                            break;
+                    }
+                }
+
+                $sort=$_GET['sort']??'1';
+                $order=$_GET['order']??'1';
+                $files=get_array_files();
+                sort_array_files($files,$sort,$order);
+
+                foreach($files as $f){
+                    $name=$f['name'];
+                    $size=$f['size'];
+                    $created=$f['created'];
+                    $type=$f['type'];
                     echo "<li>
                                 <div class=\"title\">
                                     <a href=\"/uploads/$name\">$name</a>
@@ -67,7 +130,7 @@
                                         $size
                                     </div>
                                     <div class=\"created\">
-                                        $ctime
+                                        $created
                                     </div>
                                     <div class=\"type\">
                                         $type
@@ -77,6 +140,5 @@
             }?>
         </ul>
     </main>
-    <script src=""></script>
 </body>
 </html>
